502a74d19e17fd6d61cabe6b2ef4b312d947b612
Neil Goldman <neilgoldman@outlook.com>
2016-04-16 12:02:17 -0400

    User models sort of work. Tests for user funcs

--- back/quick_scripts.py
+++ back/quick_scripts.py
@@ -1,4 +1,8 @@
 import requests
+from mongoengine import DoesNotExist
+
+from users.models import CashTagUser
+from util.mongo_utils import connect_to_mongodb_if_not_connected
 
 __author__ = '2b||!2b'
 
@@ -32,9 +36,69 @@ def get_posted_cashtag(pk):
     assert response.json()['_id'] == pk
 
 
+def create_test_users():
+    print('Creating users')
+    try:
+        test_user = CashTagUser.objects.get(username='test_user')
+    except DoesNotExist:
+        test_user = CashTagUser(
+            username='test_user',
+            password='password',
+        )
+        test_user.save()
+
+    user_info = ['neil', 'cody', 'daariel']
+    users_created = []
+    users_were_created_this_test = False
+    for username in user_info:
+        try:
+            user = CashTagUser.objects.get(username=username)
+        except DoesNotExist:
+            users_were_created_this_test = True
+            user = CashTagUser(
+                username=username,
+                password='password',
+            )
+            user.save()
+        users_created.append(user)
+
+    if users_were_created_this_test:
+        for userA in users_created:
+            for userB in users_created:
+                if userA.pk == userB.pk:
+                    continue
+                userA.friends.append(str(userB.pk))
+                userA.contacts.append(str(userB.pk))
+            userA.contacts.append(str(test_user.pk))
+            userA.save()
+    print('users created')
+
+
+def verify_test_users():
+    user_info = ['neil', 'cody', 'daariel']
+    for username in user_info:
+        user = CashTagUser.objects.get(username=username)
+        assert user.username == username
+        try:
+            assert len(user.friends) == 2
+        except AssertionError:
+            print('len(user.friends) ==')
+            print(len(user.friends))
+            exit()
+        try:
+            assert len(user.contacts) == 3
+        except AssertionError:
+            print('len(user.contacts) ==')
+            print(len(user.contacts))
+            exit()
+    print('User creatiom verified')
+
 
 if __name__ == '__main__':
     pk = post_cashtag()
     get_posted_cashtag(pk)
 
+    connect_to_mongodb_if_not_connected()
+    create_test_users()
+    verify_test_users()
 
--- back/users/models.py
+++ back/users/models.py
@@ -1,8 +1,14 @@
 import mongoengine
 
 
-# Create your models here.
 class CashTagUser(mongoengine.Document):
     username = mongoengine.StringField()
     password = mongoengine.StringField()
 
+    cashtags_contributed_to = mongoengine.ListField()
+    cashtags_watching = mongoengine.ListField()
+    cashtags_created_active = mongoengine.ListField()
+    # cashtags_created_past = mongoengine.ListField()
+
+    friends = mongoengine.ListField()
+    contacts = mongoengine.ListField()
