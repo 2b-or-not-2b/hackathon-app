59730e1ac0c59cffdacd1c5f2862cf732b09f8ad
Neil Goldman <neilgoldman@outlook.com>
2016-04-17 08:56:44 -0400

    set allowed hosts

--- back/back/settings.py
+++ back/back/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = '4ia13fw(=)teacv8-)w-x6!)cd0mdcwpzcdtyz@q#c7his4$=+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
